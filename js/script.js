d94190e14d085b4e54fc9f4e3b887f3fa64a4735
nguyen van thanh <thanhbg.tnut@gmail.com>
2019-06-13 16:30:32 +0700

    fix 4:30 130619

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  $(window).on('scroll', function (event) {
+        // alert("scroll");
+        var scrollValue = $(window).scrollTop();
+        if (scrollValue > 120) {
+            $('.menu').addClass('fixed-top');
+            //alert("scroll");
+        } else{
+            $('.menu').removeClass('fixed-top');
+        }
+    });
